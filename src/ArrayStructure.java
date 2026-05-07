651afe636ce59efb774cbbb901e42fef5f9ecb2b
Tiago Mota <tmota.seg@gmail.com>
2026-05-07 16:20:23 -0300

    Declaraçao, inicializaçao, acesso de posiçao, atualizaçao de posiçao e verificando o tamanho do array.

--- src/ArrayStructure.java
+++ src/ArrayStructure.java
@@ -2,8 +2,20 @@
 public class ArrayStructure {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declarar e Inicializar:
+		String[] tarefas = { "Estudar Java", "Fazer Exercícios", "Tomar Café" };
 
+		// Acessar Posições do Array:
+		System.out.println(tarefas[0]);
+		System.out.println(tarefas[1]);
+		System.out.println(tarefas[2]);
+
+		// Atualizar posição do Array:
+		tarefas[2] = "Limpar a Casa";
+		System.out.println(tarefas[2]);
+
+		// Tamanho do Array através do atributo 'length':
+		System.out.println("Você possui " + tarefas.length + " tarefas.");
 	}
 
 }
