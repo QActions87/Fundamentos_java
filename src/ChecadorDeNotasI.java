2d2d9cf38628a645e853218bf00e72df4ee90742
Tiago Mota <tmota.seg@gmail.com>
2026-05-24 19:29:10 -0300

    Switch-case com console.readLine();' no lugar do Scanner.

---
+++ src/ChecadorDeNotasI.java
@@ -0,0 +1,20 @@
+import java.io.Console;
+
+public class ChecadorDeNotasI {
+
+	public static void main(String[] args) {
+		Console console = System.console();
+
+		System.out.println("Informe a nota (A, B, C, D ou F)");
+		String nota = console.readLine();
+
+		switch (nota) {
+		case "A" -> System.out.println("Excelente!");
+		case "B" -> System.out.println("Bom trabalho!");
+		case "C" -> System.out.println("Está progredindo!");
+		case "D" -> System.out.println("Precisa melhorar!");
+		case "F" -> System.out.println("Você precisa estudar mais!");
+		default -> System.out.println("Nota inválida!");
+		}
+	}
+}
